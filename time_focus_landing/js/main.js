b535525abe57b43ac125628787a8a75eefe626e6
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-02-27 08:07:16 +0300

    javascritp-time-focus-landing-page

--- time_focus_landing/js/main.js
+++ time_focus_landing/js/main.js
@@ -51,6 +51,62 @@ function setBgGreet() {
     document.body.style.color = "white";
   }
 }
+
+//Get Name
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+
+//set Name
+function setName(e) {
+  if (e.type === "keypress") {
+    //Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      //setting the name after blur
+      localStorage.setItem("name", e.target.innerText);
+      //getting out of blur state
+      name.blur();
+    }
+  } else {
+    //setting the name after blur
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+//Get Focus
+function getFocus() {
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
+  } else {
+    focus.textContent = localStorage.getItem(focus);
+  }
+}
+//set Name
+function setFocus(e) {
+  if (e.type === "keypress") {
+    //Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      //setting the name after blur
+      localStorage.setItem("focus", e.target.innerText);
+      //getting out of blur state
+      focus.blur();
+    }
+  } else {
+    //setting the name after blur
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+//event listener to set the name
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
+
 //Run
 showTime();
 setBgGreet();
+getName();
+getFocus();
