ee14383770bde0bb8a2df406dd018e00b2521642
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-05-05 14:47:07 +0300

    completing-password-validation

--- 01_formvalidator/script.js
+++ 01_formvalidator/script.js
@@ -23,38 +23,95 @@ function showSuccess(input) {
 }
 
 //check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  //tests whether the String Matches the given regular expression
-  return re.test(String(email).toLowerCase());
+  //test is a JavaScript Function
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not Valid');
+  }
 }
-//Event Listeners
+
+//Check required Fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+//check Passwords Match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+//Get field Name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//CheckInput Length
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+//Ading an Event Listener on Submit
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  if (username.value === '') {
-    showError(username, 'Username is required');
-  } else {
-    showSuccess(username);
-  }
 
-  if (email.value === '') {
-    showError(email, 'Email is required');
-  } else if (!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid');
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === '') {
-    showError(password, 'Password is required');
-  } else {
-    showSuccess(password);
-  }
-
-  if (password2.value === '') {
-    showError(password2, 'Password2 is required');
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
+
+//Unclean Way
+// //Event Listeners
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   if (username.value === '') {
+//     showError(username, 'Username is required');
+//   } else {
+//     showSuccess(username);
+//   }
+
+//   if (email.value === '') {
+//     showError(email, 'Email is required');
+//   } else if (!isValidEmail(email.value)) {
+//     showError(email, 'Email is not valid');
+//   } else {
+//     showSuccess(email);
+//   }
+
+//   if (password.value === '') {
+//     showError(password, 'Password is required');
+//   } else {
+//     showSuccess(password);
+//   }
+
+//   if (password2.value === '') {
+//     showError(password2, 'Password2 is required');
+//   } else {
+//     showSuccess(password2);
+//   }
+// });
